11bc6c4271e2c55a4a1870a42413d728ded5a62a
Ariejan de Vroom <ariejan@ariejan.net>
2014-11-28 13:38:02 +0100

    Sample code and tests

---
+++ game_of_life.rb
@@ -0,0 +1,9 @@
+# Your code here...
+
+class Car
+
+  # Honks the horn
+  def honk!
+    "honk! honk!"
+  end
+end
---
+++ game_of_life_test.rb
@@ -0,0 +1,17 @@
+require 'minitest/autorun'
+require './game_of_life'
+
+# Minitest docs: http://www.rubydoc.info/gems/minitest/5.4.3/frames
+
+class GameOfLifeTest < Minitest::Test
+
+  def test_truth
+    assert true
+  end
+
+  def test_honk
+    car = Car.new
+    assert_equal "honk! honk!", car.honk!
+  end
+
+end
